a089e974a3e03016077f28c66a38303514556c19
shenliyuan <shenliyuan02@meituan.com>
2025-08-10 22:43:08 +0800

    feat: Add initial project structure with main app files and assets

---
+++ petrecord/petrecord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  petrecord
+//
+//  Created by 沈力源 on 2025/8/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ petrecord/petrecord/petrecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  petrecordApp.swift
+//  petrecord
+//
+//  Created by 沈力源 on 2025/8/10.
+//
+
+import SwiftUI
+
+@main
+struct petrecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
